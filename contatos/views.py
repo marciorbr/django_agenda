48792bb1c222275a8fc89cc5f655770115669d3d
Márcio Oliveira <marcio035@gmail.com>
2022-03-11 16:14:32 -0400

    feat: add paginator in contatos page index.html

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Contato
 
 def index(request):
     
     contatos = Contato.objects.all()
+    
+    paginator = Paginator(contatos, 10) # Mostra 10 contatos por página
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
