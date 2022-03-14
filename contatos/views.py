5b02233c150fef1c3674c4ccb68bfa497e39bb5f
Márcio Oliveira <marcio035@gmail.com>
2022-03-14 16:23:52 -0400

    feat: Add field find

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import Http404
 from .models import Contato
+from django.db.models import Q
+
+from django.contrib import messages
 
 def index(request):
     
@@ -26,4 +29,21 @@ def ver_contato(request, contato_id):
     
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+    
+def busca(request):
+    
+    termo = request.GET.get('termo')
+    
+    contatos = Contato.objects.order_by('nome').filter(
+        Q(nome__icontains=termo) | Q(sobrenome__icontains=termo),
+        exibir = True
+    )
+    
+    paginator = Paginator(contatos, 10) # Mostra 10 contatos por página
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
