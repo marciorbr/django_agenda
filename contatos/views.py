7f6d5fe75114a6286ded421ed4ff34c13fcc78bf
Márcio Oliveira <marcio035@gmail.com>
2022-03-16 16:38:28 -0400

    feat: Add custom page error when search field is empty

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.http import Http404
 from .models import Contato
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 
 from django.contrib import messages
 
@@ -37,7 +38,12 @@ def busca(request):
     termo = request.GET.get('termo')
     
     if termo is None or not termo:
-        raise Http404
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Campo de busca não pode ficar vazio.'
+        )
+        return redirect('index')
     
     campos = Concat('nome', Value(' '), 'sobrenome')
     
